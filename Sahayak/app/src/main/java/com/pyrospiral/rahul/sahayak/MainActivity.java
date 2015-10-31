1a2ecb3f2d09e7f3a036b9a8a94c11fadfc44933
KaANO-8 <kaanorahul8@gmail.com>
2015-10-31 13:18:09 +0530

    Overlay Objects Added

--- Sahayak/app/src/main/java/com/pyrospiral/rahul/sahayak/MainActivity.java
+++ Sahayak/app/src/main/java/com/pyrospiral/rahul/sahayak/MainActivity.java
@@ -1,13 +1,14 @@
 package com.pyrospiral.rahul.sahayak;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Intent svc = new Intent(this, OverlayObjects.class);
+        startService(svc);
+        finish();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
